45bcfd824ced62fb9b970351fd86137a671df59e
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-11-16 00:42:23 +0100

    First MVP working version

--- example/App.tsx
+++ example/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Stack from '../src/Stack';
 
 function App() {
   return (
-    <div style={{ backgroundColor: 'red', width: '40rem', height: '40rem' }}>
-      Demo
-    </div>
+    <>
+      <h1>Demo website</h1>
+      <Stack project="https://www.stack-ai.com/embed/46bf5b6a-9b4d-48f6-8a13-cdfc4fe58520/11da0c81-afe2-4ccd-b498-807bbde8e7f1/653fefcfcc37c0093d55e6a9" />
+    </>
   );
 }
 
---
+++ src/Stack.tsx
@@ -0,0 +1,66 @@
+import { forwardRef, useEffect, LegacyRef } from 'react';
+
+type StackProps = {
+  project: string;
+  innerRef?: LegacyRef<HTMLIFrameElement> | undefined;
+};
+
+// We are forwarding the ref to the iframe so that the user has access to it.
+const Stack = forwardRef(function Stack({ project, innerRef }: StackProps) {
+  // Resizes based on the open/close state of the chatbot
+  useEffect(() => {
+    const iframe = document.getElementById('responsiveIframe');
+    if (iframe) {
+      iframe.style.width = '90px';
+      iframe.style.height = '90px';
+    }
+
+    const handleMessage = (event: MessageEvent) => {
+      const iframe = document.getElementById('responsiveIframe');
+      if (iframe && event.data.type === 'chatbotStateChange') {
+        if (iframe && event.data.isClosed) {
+          setTimeout(() => {
+            iframe.style.width = '90px';
+            iframe.style.height = '90px';
+          }, 300);
+        } else {
+          const isMobile = window.innerWidth < 768;
+          if (isMobile) {
+            // Mobile
+            iframe.style.width = '100vw';
+            iframe.style.height = '38.5rem';
+          } else {
+            // Desktop
+            iframe.style.width = '35rem';
+            iframe.style.height = '38.5rem';
+          }
+        }
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
+  return (
+    <iframe
+      ref={innerRef}
+      id="responsiveIframe"
+      className="chatbot-container"
+      src={project}
+      style={{
+        position: 'fixed',
+        zIndex: '100',
+        overflow: 'hidden',
+        bottom: '0',
+        right: '0',
+        border: 'none',
+        borderRadius: '10px',
+      }}
+    />
+  );
+});
+
+export default Stack;
